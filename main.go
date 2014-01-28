fa1711f6043b43d3e8935c66c88338a438f3e006
toby.zxj <toby.zxj@gmail.com>
2014-01-28 17:22:21 +0800

    modify compile error.
    Signed-off-by: toby.zxj <toby.zxj@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	_ "github.com/tobyzxj/passwordkeeper/passwordkeeper/routers"
 	"github.com/astaxie/beego"
+	_ "github.com/tobyzxj/passwordkeeper/routers"
 )
 
 func main() {
 	beego.Run()
 }
-
